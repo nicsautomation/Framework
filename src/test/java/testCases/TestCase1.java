ba0074f046c346883acb4539db0304d6c2147b47
nicsautomation <newitcareersolutions@gmail.com>
2022-09-24 22:47:13 -0400

    project changes

--- src/test/java/testCases/TestCase1.java
+++ src/test/java/testCases/TestCase1.java
@@ -3,6 +3,6 @@ package testCases;
 public class TestCase1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("hello, doing projcet changes");
 	}
 }
