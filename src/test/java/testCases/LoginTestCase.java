0d420042ad956b16f57be81e0725eaa4c5789fa6
Gurminder-Boparai <gsboparai90@gmail.com>
2022-09-25 20:24:24 -0400

    Second Update to login By Gurminder In FeartureLogin

--- src/test/java/testCases/LoginTestCase.java
+++ src/test/java/testCases/LoginTestCase.java
@@ -12,15 +12,26 @@ import utilities.DriverClass;
 public class LoginTestCase extends DriverClass {
 	static WebDriver driver;
 	
-	public static void main(String args[])
+	public static void main(String args[]) throws InterruptedException
 	{
 		driver=Init_Driver();
-		driver.get("http://demo.testfire.net/login.jsp");
-		driver.findElement(By.id("uid")).sendKeys("jsmith");
-		driver.findElement(By.id("passw")).sendKeys("Demo1234");
-		driver.findElement(By.xpath("//input[@name=\"btnSubmit\"]")).click();
-		String expURL="http://demo.testfire.net/bank/main.jsp";
-		String actURL=driver.getCurrentUrl();
-		Assert.assertEquals(actURL, expURL, "InCorrect Main Page");
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		String expURL="https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList!!!!";
+		String actURL= driver.getCurrentUrl();
+		Assert.assertEquals(actURL, expURL, "Incorrect Home Page!!!" );
+		
+		/*
+		 * driver.get("http://demo.testfire.net/login.jsp");
+		 * driver.findElement(By.id("uid")).sendKeys("jsmith");
+		 * driver.findElement(By.id("passw")).sendKeys("Demo1234");
+		 * driver.findElement(By.xpath("//input[@name=\"btnSubmit\"]")).click(); String
+		 * expURL="http://demo.testfire.net/bank/main.jsp"; String
+		 * actURL=driver.getCurrentUrl(); Assert.assertEquals(actURL, expURL,
+		 * "InCorrect Main Page");
+		 */
 	}
 }
